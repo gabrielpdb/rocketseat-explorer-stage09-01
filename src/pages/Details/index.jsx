7d8764f42739fa62412446756ebe2eef364ca35e
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-05 09:39:08 -0300

    CSS in JS

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import './styles.css'
+import { Container } from './styles'
 
 export function Details() {
   return (
-    <>
+    <Container>
       <h1>Hello</h1>
       <span>Yo</span>
-    </>
+    </Container>
   )
 }
