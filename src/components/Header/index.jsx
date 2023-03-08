f496a0d0f4671c7f4eec01c05af5448d9f132a3b
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-07 21:26:20 -0300

    Imagem do usuário

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,16 @@
-import { Container } from './styles'
+import { Container, Profile } from './styles'
 
 export function Header() {
-  return <Container></Container>
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/gabrielpdb.png" alt="Imagem de Gabriel" />
+
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Gabriel Dal Bó</strong>
+        </div>
+      </Profile>
+    </Container>
+  )
 }
