0779c52b3c1c3c6d7f226664da1fea47ff5a41df
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-07 21:42:12 -0300

    Children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,26 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
