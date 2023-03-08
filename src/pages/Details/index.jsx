120afc7d857b72b411926c51bdddcffd64e6e2b8
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-07 21:53:21 -0300

    Componente Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
@@ -20,6 +21,11 @@ export function Details() {
             <a href="#">Link 1</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
       <Button title="Voltar" />
     </Container>
