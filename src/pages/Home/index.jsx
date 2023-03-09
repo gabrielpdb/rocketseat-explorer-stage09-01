1ec65add8590c152466f955fbfad42745bc6b9f2
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-08 22:15:30 -0300

    Input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,9 @@
-import { FiPlus } from 'react-icons/fi'
+import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
+import { Input } from '../../components/Input'
 
 export function Home() {
   return (
@@ -25,7 +26,9 @@ export function Home() {
         </li>
       </Menu>
 
-      <Search></Search>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+      </Search>
 
       <Content></Content>
 
