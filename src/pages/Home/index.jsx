bf13b3fb06148fab2f41a753f18007ba5845cd83
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-03-08 22:03:54 -0300

    Filtros

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
   return (
@@ -11,7 +12,17 @@ export function Home() {
 
       <Header />
 
-      <Menu></Menu>
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
+      </Menu>
 
       <Search></Search>
 
