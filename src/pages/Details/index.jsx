7bc5459e95d45e3d7c9ea77ff5e118b1969ca4af
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-21 20:24:47 -0300

    grid to home page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,29 +1,45 @@
 import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title={'Links úteis'}>
-        <Links>
-          <li>
-            <a href="#">Link 1</a>
-          </li>
-          <li>Item</li>
-          <li>Item</li>
-          <li>Item</li>
-        </Links>
-      </Section>
-      <Section title={'Marcadores'}>
-        <Tag title={'express'} />
-        <Tag title={'node'} />
-      </Section>
-      <Button title="Voltar" />
+      <main>
+        <Content>
+          <ButtonText title={'Excluir nota'} />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+            debitis incidunt quam eligendi praesentium inventore error
+            voluptatibus, rem corporis pariatur illum nostrum recusandae eum.
+            Quod laboriosam obcaecati a voluptas repellendus.
+          </p>
+
+          <Section title={'Links úteis'}>
+            <Links>
+              <li>
+                <a href="#">Link 1</a>
+              </li>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Item</li>
+            </Links>
+          </Section>
+          <Section title={'Marcadores'}>
+            <Tag title={'express'} />
+            <Tag title={'node'} />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
