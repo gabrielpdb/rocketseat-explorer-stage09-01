3a3edc9e379b03ee45c4dc11694668c6443333f8
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-22 21:03:35 -0300

    Profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,34 @@
+import { FiArrowLeft, FiCamera, FiLock, FiMail, FiUser } from 'react-icons/fi'
+import { Container, Form, Avatar } from './styles'
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img src="https://github.com/gabrielpdb.png" alt="Foto do usuário" />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input type="file" id="avatar" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="Senha atual" type="password" icon={FiLock} />
+        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+
+        <Button title={'Salvar'} />
+      </Form>
+    </Container>
+  )
+}
