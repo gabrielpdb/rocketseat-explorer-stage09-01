244f3bec28e0199e89a141f8f310a4decbbffccd
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-23 21:14:17 -0300

    done

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,6 +5,8 @@ import { Section } from '../../components/Section'
 import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 
+import { Link } from 'react-router-dom'
+
 import { Container, Form } from './styles'
 
 export function New() {
@@ -16,7 +18,7 @@ export function New() {
         <Form>
           <header>
             <h1>Criar nota</h1>
-            <a href="/">voltar</a>
+            <Link to="/">voltar</Link>
           </header>
 
           <Input placeholder="Título" />
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,6 +3,7 @@ import { Background, Container, Form } from './styles'
 import { FiLock, FiMail } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { Link } from 'react-router-dom'
 
 export function SignIn() {
   return (
@@ -18,7 +19,7 @@ export function SignIn() {
 
         <Button title={'Entrar'} />
 
-        <a href="#">Criar conta</a>
+        <Link to="/register">Criar conta</Link>
       </Form>
 
       <Background />
