2131f3f42ea5c4704ab5615555fce230fa63d523
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-08-23 21:01:18 -0300

    Interface done

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,42 @@
+import { Input } from '../../components/Input'
+import { Header } from '../../components/Header'
+import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
+import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+
+import { Container, Form } from './styles'
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
+
+          <Section title={'Links úteis'}>
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title={'Marcadores'}>
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title={'Salvar'} />
+        </Form>
+      </main>
+    </Container>
+  )
+}
